678d7b67a666946a40a29e1c2d7dda8474e775fd
ChristopherChacon <chris.chacon6@gmail.com>
2022-05-15 11:50:02 -0400

    Adding all Files

---
+++ Chris/Actual Coding/Load_Cleaner_Data.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import numpy as np
+import os
+
+def load_dataset(fname=None):
+    if fname is None:
+        fname = os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir))
+        fname = os.path.join(fname, 'labels.csv')
+        
+    data = np.genfromtxt(fname, dtype=['|S19', '<f8', '|S4'], names=[
+                         'path', 'probability', 'type'])
+    i = 0
+    while(i < len(data)):
+        if not (data['probability'][i] == 0.0 or data['probability'][i] == 1.0):
+            data = np.delete(data, i)
+            i -= 1
+        i += 1
+    
+    image_fnames = np.char.decode(data['path'])
+    probs = data['probability']
+
+    def load_cell_image(fname):
+        
+        with Image.open(fname) as image:
+            return np.asarray(image)
+
+    dir = os.path.dirname(fname)
+
+    images = np.array([load_cell_image(os.path.join(dir, fn))
+                       for fn in image_fnames])
+
+    return images, probs
